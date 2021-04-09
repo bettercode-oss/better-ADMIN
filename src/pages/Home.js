cf18c5a7b91f2b6dbadd3b10192a4aa3a9311d08
yoonjongpark <yoonjongpark@gmail.com>
2021-04-09 14:25:22 +0900

    #13 Dashboard 제작
    bug fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,63 @@
 import React from "react";
 
-const Home = () => (
-  <>
-    <div>
-      <h1>Home page</h1>
-    </div>
-  </>
-);
+import {
+  faUsers,
+  faBuilding,
+  faCalendar,
+  faQuestion,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import styles from "./Home.module.css";
+
+const Home = () => {
+  //TODO: 중요 미처리 건 색상 변경용. 추후 api 연동.
+  let stateEmergency = false;
+
+  const emergencyColor = (state) => {
+    if (state === true) {
+      return styles.summryModule;
+    } else {
+      return styles.summryModuleEmergency;
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <section id="summury" className={styles.item}>
+          <div className={styles.summryModule}>
+            <FontAwesomeIcon icon={faUsers} size="3x" />
+            <div className={styles.summuryText}>
+              <span className={styles.summuryTitle}>총 가입자</span>
+              <span className={styles.summuryCount}>10,001</span>
+            </div>
+          </div>
+          <div className={styles.summryModule}>
+            <FontAwesomeIcon icon={faBuilding} size="3x" />
+            <div className={styles.summuryText}>
+              <span className={styles.summuryTitle}>등록 기업</span>
+              <span className={styles.summuryCount}>2,000</span>
+            </div>
+          </div>
+          <div className={styles.summryModule}>
+            <FontAwesomeIcon icon={faCalendar} size="3x" />
+            <div className={styles.summuryText}>
+              <span className={styles.summuryTitle}>진행중 캠페인</span>
+              <span className={styles.summuryCount}>5</span>
+            </div>
+          </div>
+          <div className={emergencyColor(stateEmergency)}>
+            <FontAwesomeIcon icon={faQuestion} size="3x" />
+            <div className={styles.summuryText}>
+              <span className={styles.summuryTitle}>미처리 문의</span>
+              <span className={styles.summuryCount}>2</span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default Home;
