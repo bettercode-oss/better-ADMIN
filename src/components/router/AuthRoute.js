29b4c216a0e0190c898329773cc43fa6dc9d472b
Young-mo Yoo <gigamadness@gmail.com>
2021-11-12 16:13:51 +0900

    #100 로그인한 상태에서 브라우저를 새로고침 하는 경우 로그인 사용자 정보(memberContext) 초기화 문제

--- src/components/router/AuthRoute.js
+++ src/components/router/AuthRoute.js
@@ -2,10 +2,16 @@ import React, {useEffect, useState} from 'react';
 import {Route, useHistory} from 'react-router-dom';
 import {adminConfig} from "../../config/admin.config";
 import {AuthService} from "../../auth/auth.service";
+import {
+  CHANGE_MEMBER_CONTEXT_EVENT_TOPIC,
+  EventBroadcaster,
+  MEMBER_CONTEXT_AVAILABLE_EVENT_TOPIC
+} from "../../event/event.broadcaster";
 
 export default function AuthRoute({path, exact = false, component}) {
   const [authenticated, setAuthenticated] = useState(false);
   const history = useHistory();
+  const [memberContextAvailable, setMemberContextAvailable] = useState(false);
 
   const checkAuth = () => {
     AuthService.checkAuth().then(() => {
@@ -17,7 +23,15 @@ export default function AuthRoute({path, exact = false, component}) {
 
   useEffect(() => {
     checkAuth();
+
+    EventBroadcaster.on(MEMBER_CONTEXT_AVAILABLE_EVENT_TOPIC, (memberInformation) => {
+      setMemberContextAvailable(true);
+      EventBroadcaster.broadcast(
+        CHANGE_MEMBER_CONTEXT_EVENT_TOPIC,
+        memberInformation
+      );
+    });
   });
 
-  return <Route exact={exact} path={path} component={authenticated && component}/>;
+  return <Route exact={exact} path={path} component={(authenticated && memberContextAvailable) && component}/>;
 }
