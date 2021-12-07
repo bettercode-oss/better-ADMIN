d830631b542ab749927d97b286ddf33fe4715eaf
Young-mo Yoo <gigamadness@gmail.com>
2021-12-07 18:13:00 +0900

    #78 멤버 목록 화면에서 상세나 편집으로 이동 후 다시 돌아왔을 때 기존 페이지와 검색 조건, 필터 유지 시키기

--- src/components/settings/member/MemberList.js
+++ src/components/settings/member/MemberList.js
@@ -1,33 +1,54 @@
 import React, {useEffect, useState} from "react";
 import {Button, Collapse, Dropdown, Input, Menu, PageHeader, Space, Table, Tag} from 'antd';
 import {MemberService} from "./member.service";
-import {DownOutlined, SettingOutlined, SearchOutlined} from "@ant-design/icons";
+import {DownOutlined, SearchOutlined, SettingOutlined} from "@ant-design/icons";
 
-const { Panel } = Collapse;
-const { Column } = Table;
+const {Panel} = Collapse;
+const {Column} = Table;
 const PAGE_SIZE = 5;
 
-let searchText = null;
-
-const MemberList = ({onRoleChange}) => {
+const MemberList = ({initSearchCondition, onRoleChange}) => {
   const [members, setMembers] = useState([]);
   const [memberTotalCount, setMemberTotalCount] = useState(0);
   const [searchFilters, setSearchFilters] = useState(null);
 
+  // 검색 조건
+  const [searchCondition, setSearchCondition] = useState(initSearchCondition);
+
   useEffect(() => {
     loadSearchFilters();
-    loadMembers({
-      page: 1
-    });
   }, []);
 
-  const loadMembers = (params) => {
+  useEffect(() => {
+    const params = {};
+    if(searchCondition) {
+      if(searchCondition.pagination) {
+        params.page = searchCondition.pagination.current;
+      }
+
+      if (searchCondition.filters) {
+        if (searchCondition.filters.types) {
+          params.types = searchCondition.filters.types.join(",");
+        }
+
+        if (searchCondition.filters.roles) {
+          params.roleIds = searchCondition.filters.roles.join(",");
+        }
+      }
+
+      if (searchCondition.searchText && searchCondition.searchText.length > 0) {
+        params.name = searchCondition.searchText;
+      }
+    } else {
+      params.page = 1;
+    }
+
     params.pageSize = PAGE_SIZE;
     MemberService.getMembersApproved(params).then(response => {
       setMembers(response.data.result);
       setMemberTotalCount(response.data.totalCount);
     });
-  }
+  }, [searchCondition]);
 
   const loadSearchFilters = () => {
     MemberService.getSearchFilters().then(response => {
@@ -36,64 +57,60 @@ const MemberList = ({onRoleChange}) => {
   }
 
   const getColumnSearchProps = dataIndex => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-      <div style={{ padding: 8 }}>
+    filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => (
+      <div style={{padding: 8}}>
         <Input
+          defaultValue={(searchCondition && searchCondition.searchText) ? searchCondition.searchText : null}
           value={selectedKeys[0]}
           onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
+          style={{marginBottom: 8, display: 'block'}}
         />
         <Space>
           <Button
             type="primary"
             onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
+            icon={<SearchOutlined/>}
             size="small"
-            style={{ width: 90 }}
+            style={{width: 90}}
           >
             찾기
           </Button>
-          <Button onClick={() => handleSearchReset(confirm, clearFilters)} size="small" style={{ width: 90 }}>
+          <Button onClick={() => handleSearchReset(confirm, clearFilters)} size="small" style={{width: 90}}>
             초기화
           </Button>
         </Space>
       </div>
     ),
-    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    filterIcon: filtered => <SearchOutlined style={{color: (searchCondition && searchCondition.searchText) ? '#1890ff' : undefined}}/>
   });
 
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    searchText = selectedKeys[0];
-    confirm();
+    setSearchCondition({
+      ...searchCondition,
+      searchText: selectedKeys[0],
+    });
   };
 
   const handleSearchReset = (confirm, clearFilters) => {
     clearFilters();
-    searchText = null;
-    confirm();
+    setSearchCondition({
+      ...searchCondition,
+      searchText: null,
+    });
   };
 
   const memberTableChanged = (pagination, filters) => {
-    const params = {
-      page: pagination.current,
-    };
+    setSearchCondition({
+      ...searchCondition,
+      pagination: pagination,
+      filters: filters
+    });
+  }
 
-    if(filters) {
-      if(filters.types) {
-        params.types = filters.types.join(",");
-      }
 
-      if(filters.roles) {
-        params.roleIds = filters.roles.join(",");
-      }
-    }
-
-    if(searchText && searchText.length > 0) {
-      params.name = searchText;
-    }
-
-    loadMembers(params);
+  const handleRoleChange = (member) => {
+    onRoleChange(member, searchCondition);
   }
 
   return (
@@ -103,49 +120,58 @@ const MemberList = ({onRoleChange}) => {
         subTitle="멤버에 역할을 할당하거나 삭제합니다."
       >
         {searchFilters &&
-        <Table rowKey="id" dataSource={members} locale={{emptyText: "데이터 없음"}}
-               pagination={{pageSize: PAGE_SIZE, total: memberTotalCount}}
-               onChange={memberTableChanged}>
-          <Column title="이름" dataIndex="name" key="name" render={(text, record) => <span>{text}({record.candidateId})</span>}
-                  {...getColumnSearchProps('name')}/>
-          <Column title="유형" dataIndex="typeName" key="types"
-                  filters={(searchFilters && searchFilters.length > 0) ? searchFilters.filter(s => s.name === 'type')[0].filters : []}
-                  render={(text, record) => <Tag color={record.type === 'site' ? 'magenta' : 'blue'}>{text}</Tag>} />
-          <Column title="멤버 역할" dataIndex="roles" key="roles" width="200px"
-                  filters={(searchFilters && searchFilters.length > 0) ? searchFilters.filter(s => s.name === 'role')[0].filters : []}
-                  render={(text, record) => {
-            return <div>
-              {record.roles.map(role => (
-                <Tag key={role.id} color="orange">{role.name}</Tag>
-              ))}
-            </div>
-          }} />
-          <Column title="소속 조직" render={(text, record) => {
-            if(record.organizations) {
-              return <Collapse ghost>
-                {record.organizations.map((organization) => {
-                  return <Panel header={organization.name} key={organization.id}>
-                    {organization.roles && organization.roles.map(role =>
-                      (<Tag key={role.id} color="orange">{role.name}</Tag>))}
-                  </Panel>
-                })
-                }</Collapse>
-            }
-          }} />
-          <Column title="" align='right' render={(text, record) => {
-            return (
-              <Dropdown overlay={
-                <Menu>
-                  <Menu.Item key="0">
-                    <Button type="text" onClick={() => {onRoleChange(record)}}>멤버 역할 변경</Button>
-                  </Menu.Item>
-                </Menu>} trigger={['click']}>
-                <Button style={{borderRadius: '5px'}} icon={<SettingOutlined/>}>
-                  <DownOutlined/>
-                </Button>
-              </Dropdown>)
-          }} />
-        </Table>}
+          <Table rowKey="id" dataSource={members} locale={{emptyText: "데이터 없음"}}
+                 pagination={{
+                   current: (searchCondition && searchCondition.pagination && searchCondition.pagination.current) ? searchCondition.pagination.current : 1,
+                   pageSize: PAGE_SIZE,
+                   total: memberTotalCount
+                 }}
+                 onChange={memberTableChanged}>
+            <Column title="이름" dataIndex="name" key="name"
+                    render={(text, record) => <span>{text}({record.candidateId})</span>}
+                    {...getColumnSearchProps('name')}/>
+            <Column title="유형" dataIndex="typeName" key="types"
+                    defaultFilteredValue={(searchCondition && searchCondition.filters && searchCondition.filters.types) ? searchCondition.filters.types : null}
+                    filters={(searchFilters && searchFilters.length > 0) ? searchFilters.filter(s => s.name === 'type')[0].filters : []}
+                    render={(text, record) => <Tag color={record.type === 'site' ? 'magenta' : 'blue'}>{text}</Tag>}/>
+            <Column title="멤버 역할" dataIndex="roles" key="roles" width="200px"
+                    defaultFilteredValue={(searchCondition && searchCondition.filters && searchCondition.filters.roles) ? searchCondition.filters.roles : null}
+                    filters={(searchFilters && searchFilters.length > 0) ? searchFilters.filter(s => s.name === 'role')[0].filters : []}
+                    render={(text, record) => {
+                      return <div>
+                        {record.roles.map(role => (
+                          <Tag key={role.id} color="orange">{role.name}</Tag>
+                        ))}
+                      </div>
+                    }}/>
+            <Column title="소속 조직" render={(text, record) => {
+              if (record.organizations) {
+                return <Collapse ghost>
+                  {record.organizations.map((organization) => {
+                    return <Panel header={organization.name} key={organization.id}>
+                      {organization.roles && organization.roles.map(role =>
+                        (<Tag key={role.id} color="orange">{role.name}</Tag>))}
+                    </Panel>
+                  })
+                  }</Collapse>
+              }
+            }}/>
+            <Column title="" align='right' render={(text, record) => {
+              return (
+                <Dropdown overlay={
+                  <Menu>
+                    <Menu.Item key="0">
+                      <Button type="text" onClick={() => {
+                        handleRoleChange(record)
+                      }}>멤버 역할 변경</Button>
+                    </Menu.Item>
+                  </Menu>} trigger={['click']}>
+                  <Button style={{borderRadius: '5px'}} icon={<SettingOutlined/>}>
+                    <DownOutlined/>
+                  </Button>
+                </Dropdown>)
+            }}/>
+          </Table>}
       </PageHeader>
     </>
   )
