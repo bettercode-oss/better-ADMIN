6b8af64df8d73e465835dfab00fac3fd13b945f2
Young-mo Yoo <gigamadness@gmail.com>
2022-02-04 18:34:57 +0900

    #141 App.js useCallback 사용 코드 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from "react";
+import React, {useEffect} from "react";
 import {HashRouter, Route, Switch} from "react-router-dom";
 import './App.less';
 import AppLayout from "./components/layout/AppLayout";
@@ -17,19 +17,15 @@ import {LayoutProvider} from "./components/layout/AppLayoutContext";
 import OAuthLoginResult from "./components/login/OAuthLoginResult";
 
 const App = () => {
-  const existQueryStringInUrl = (url) => {
-    return url.split("?")[1] ? true : false;
-  }
-
-  const getQueryStringInUrl = useCallback((url) => {
-    if (existQueryStringInUrl(url)) {
-      return url.split("?")[1];
-    } else {
-      return null;
-    }
-  }, []);
-
   useEffect(() => {
+    const getQueryStringInUrl = (url) => {
+      if (existQueryStringInUrl(url)) {
+        return url.split("?")[1];
+      } else {
+        return null;
+      }
+    };
+
     if (adminConfig.authentication.used) {
       AuthService.silentRefresh().then().catch(() => {
         const queryString = getQueryStringInUrl(window.location.href)
@@ -58,8 +54,11 @@ const App = () => {
         icon: <NotificationTwoTone />,
       });
     });
-  }, [getQueryStringInUrl]);
+  }, []);
 
+  const existQueryStringInUrl = (url) => {
+    return url.split("?")[1] ? true : false;
+  }
 
   return (<>
     <LayoutProvider>
