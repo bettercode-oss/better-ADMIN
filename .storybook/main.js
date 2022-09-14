959e2cb6c336c28562e651e04b214c9d992df756
Young-mo Yoo <gigamadness@gmail.com>
2022-09-14 14:46:46 +0900

    #172 Design Component 개발을 위한 Storybook.js 적용하기

---
+++ .storybook/main.js
@@ -0,0 +1,54 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        craOverrides: {
+          fileLoaderExcludes: ['less'],
+        },
+      },
+    },
+  ],
+  webpackFinal: async (config) => {
+    // https://bits-of-code.medium.com/integrating-storybook-6-in-a-react-with-ant-d-4-and-typescript-6d5a8178f434
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    });
+
+    // Return the altered config
+    return {
+      ...config,
+      plugins: config.plugins.filter((plugin) => {
+        if (plugin.constructor.name === 'ESLintWebpackPlugin') {
+          return false;
+        }
+        return true;
+      }),
+    };
+  },
+  "framework": "@storybook/react"
+}
