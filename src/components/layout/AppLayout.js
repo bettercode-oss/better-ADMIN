e64c3953ffdc6c6f6092918db09af801f91a970f
Young-mo Yoo <gigamadness@gmail.com>
2022-09-15 12:03:12 +0900

    #174 react-app-version-checker 적용하기

--- src/components/layout/AppLayout.js
+++ src/components/layout/AppLayout.js
@@ -10,6 +10,8 @@ import {useLayoutDispatch, useLayoutState} from "./AppLayoutContext";
 import {useLocation, useNavigate} from "react-router-dom";
 import NavigationConfig from "../../config/navigation.config";
 import {MemberContext} from "../../auth/member.context";
+import {AppVersionChecker} from "@bettercode/react-app-version-checker";
+import {adminConfig} from "../../config/admin.config";
 
 const AppLayout = () => {
   const layoutDispatch = useLayoutDispatch();
@@ -52,18 +54,23 @@ const AppLayout = () => {
   }, [layoutDispatch]);
 
   return (
-    <Layout
-      style={{
-        minHeight: "100%"
-      }}
-    >
-      <Sider/>
-      <Layout className="site-layout">
-        <Header/>
-        <Content/>
-        <Footer/>
+    <>
+      <AppVersionChecker versionApiEndPoint={`${adminConfig.authentication.authAPI()}/site/settings/app-version`}
+                         message="새로운 버전으로 업데이트되었습니다. 최신 버전을 이용하려면 '확인'을 눌러 주세요." minuteInterval={30}
+                         okText='확인'/>
+      <Layout
+        style={{
+          minHeight: "100%"
+        }}
+      >
+        <Sider/>
+        <Layout className="site-layout">
+          <Header/>
+          <Content/>
+          <Footer/>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   );
 };
 
