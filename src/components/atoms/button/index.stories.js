5ea2b3bc4ccba64d8dd76fc357345d6cb7843cd7
torouso <torouso@naver.com>
2022-09-27 09:14:27 +0900

    #179 Button Component 제작하기

--- src/components/atoms/button/index.stories.js
+++ src/components/atoms/button/index.stories.js
@@ -4,12 +4,21 @@ import {Button} from './index';
 export default {
   title: 'atoms/Button',
   component: Button,
+  argTypes: {
+    // text: { defaultValue: 'button!!!', control: 'text' },
+    size: { options: ['small','medium','large'], control: { type: 'radio' } },
+    type: {options: ['primary','default','dashed','link','text'], control: {type: 'select'}},
+    // className: { control: 'className' },
+    // color: { defaultValue: '#abd', control: 'color' },
+  },
 };
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Button {...args} tooltipTitle={args.tooltip}/>;
 
 export const Primary = Template.bind({});
+
 Primary.args = {
   type: 'primary',
   label: 'Button',
+  size: 'medium'
 };
---
+++ src/shared/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+// import { color, typography } from './styles';
+
+export const fontUrl = 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900';
+
+export const bodyStyles = css`
+  /* Same as before */
+`;
+
+export const GlobalStyle = createGlobalStyle`
+ body {
+   ${bodyStyles}
+ }`;
