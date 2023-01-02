b37c8b76683dc698780d078f552456ad8b11c311
Young-mo Yoo <gigamadness@gmail.com>
2023-01-02 15:17:42 +0900

    #240 Layout Footer를 Admin Design Component로 대체한다

--- src/components/atoms/layout/header/index.js
+++ src/components/atoms/layout/header/index.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Layout as AntLayout } from 'antd';
+import {Layout as AntLayout} from 'antd';
 import PropTypes from "prop-types";
 
 const DEFAULT_BACKGROUND_COLOR = "#fff";
 
 export const Header = ({ backgroundColor, children }) => {
-  return <AntLayout.Header style={{background: backgroundColor ? backgroundColor : DEFAULT_BACKGROUND_COLOR, paddingInline: "10px"}}>{children}</AntLayout.Header>;
+  return <AntLayout.Header style={{background: backgroundColor ? backgroundColor : DEFAULT_BACKGROUND_COLOR, paddingInline: "10px"}}>
+    {children}
+  </AntLayout.Header>;
 };
 
-Text.propTypes = {
+Header.propTypes = {
   backgroundColor: PropTypes.string,
   children: PropTypes.any
 };
 
-
+Header.defaultProps = {
+  backgroundColor: DEFAULT_BACKGROUND_COLOR
+};
 
