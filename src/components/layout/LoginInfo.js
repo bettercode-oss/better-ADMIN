3edbb3b08a48ff2fe1e5333e729888c8f3268502
unknown <sytp93@gmail.com>
2023-04-13 15:13:34 +0900

    \#276 로그인 정보 클릭 시 나오는 dropdown 메뉴에 profile 모달 추가

--- src/components/layout/LoginInfo.js
+++ src/components/layout/LoginInfo.js
@@ -1,15 +1,17 @@
-import React, {useEffect} from "react";
-import {LogoutOutlined} from "@ant-design/icons";
-import {useNavigate} from "react-router-dom";
-import {adminConfig} from "../../config/admin.config";
-import {MemberContext} from "../../auth/member.context";
-import {AuthService} from "../../auth/auth.service";
-import {EventBroadcaster, INVALID_ACCESS_TOKEN_TOPIC} from "../../event/event.broadcaster";
-import {PageTabStorage} from "./page.tab.storage";
-import {Avatar} from "../atoms/avatar";
-import {Dropdown} from "../atoms/dropdown";
+import React, { useEffect, useState } from 'react';
+import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { useNavigate } from 'react-router-dom';
+import { adminConfig } from '../../config/admin.config';
+import { MemberContext } from '../../auth/member.context';
+import { AuthService } from '../../auth/auth.service';
+import { EventBroadcaster, INVALID_ACCESS_TOKEN_TOPIC } from '../../event/event.broadcaster';
+import { PageTabStorage } from './page.tab.storage';
+import { Avatar } from '../atoms/avatar';
+import { Dropdown } from '../atoms/dropdown';
+import Profile from '../templates/profile/Profile';
 
-const LoginInfo = () => {
+function LoginInfo() {
+  const [showProfileModal, setShowProfileModal] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,23 +30,38 @@ const LoginInfo = () => {
     });
   };
 
-  const generateMenuItems = () => {
-    return [{
-      label: '로그아웃',
-      key: '0',
-      icon: <LogoutOutlined/>,
-    }];
+  const showProfile = () => {
+    setShowProfileModal(true);
+  };
+
+  const closeProfile = () => {
+    setShowProfileModal(false);
   }
+
+  const generateMenuItems = () => [{
+    label: '로그아웃',
+    key: '0',
+    icon: <LogoutOutlined />,
+  },
+  {
+    label: '프로필',
+    key: '1',
+    icon: <UserOutlined />,
+  },
+  ];
 
   const handleMenuClick = (e) => {
     switch (e.key) {
       case '0':
         logout();
         break;
+      case '1':
+        showProfile();
+        break;
       default:
         console.log('unknown menu item');
     }
-  }
+  };
 
   return (
     <>
@@ -52,12 +69,16 @@ const LoginInfo = () => {
         items={generateMenuItems()}
         onClick={handleMenuClick}
       >
-        <Avatar ssrc={MemberContext.memberInformation.picture}
-                suffix={MemberContext.memberInformation && MemberContext.memberInformation.name}/>
+        <Avatar
+          ssrc={MemberContext.memberInformation.picture}
+          suffix={MemberContext.memberInformation && MemberContext.memberInformation.name} />
       </Dropdown>
+      <Profile
+        show={showProfileModal}
+        onClose={closeProfile}
+      />
     </>
-  )
-};
+  );
+}
 
 export default LoginInfo;
-
