fc3e432ffa82c6f246f8f55dcb04fb1b2f2b3936
unknown <sytp93@gmail.com>
2023-04-17 13:31:01 +0900

    \#279 Profile React Component의 Jest 단위 테스트를 작성한다.

---
+++ src/components/templates/profile/Profile.test.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import Profile from './Profile';
+import '@testing-library/jest-dom';
+
+jest.mock('../../../auth/member.context', () => ({
+  __esModule: true,
+  MemberContext: {
+    memberInformation: {
+      name: '관계자',
+      typeName: '시스템',
+      picture: 'https://example.com/profile.jpg',
+      roles: ['시스템 관리자', '조직/멤버 관리자'],
+      permissions: ['MANAGE_SYSTEM_SETTINGS', 'MANAGE_MEMBERS'],
+    },
+  }
+}));
+
+const Member = require('../../../auth/member.context');
+
+describe('프로필 이미지 랜더링', () => {
+  test('member context에 picture가 빈 값이면 디폴트 이미지가 나온다.', async () => {
+    // given
+    Member.MemberContext.memberInformation.picture = '';
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-image'));
+    // then
+    const avatarComp = screen.getByTestId('profile-image');
+    const profileImage = avatarComp.querySelector('img');
+
+    expect(profileImage).toHaveAttribute('src', '');
+  });
+
+  test('member context에 picture가 null 이면 디폴트 이미지가 나온다.', async () => {
+    // given
+    Member.MemberContext.memberInformation.picture = null;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-image'));
+    // then
+    const avatarComp = screen.getByTestId('profile-image');
+    const profileImage = avatarComp.querySelector('svg');
+
+    expect(profileImage).toBeInTheDocument();
+    expect(profileImage).toHaveAttribute('data-icon', 'user');
+  });
+
+  test('member context에 picture가 undefined 면 디폴트 이미지가 나온다.', async () => {
+    // given
+    Member.MemberContext.memberInformation.picture = undefined;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-image'));
+    // then
+    const avatarComp = screen.getByTestId('profile-image');
+    const profileImage = avatarComp.querySelector('svg');
+
+    expect(profileImage).toBeInTheDocument();
+    expect(profileImage).toHaveAttribute('data-icon', 'user');
+  });
+
+
+  test('member context에 picture가 url이 있으면 해당 url의 이미지를 보여 준다.', async () => {
+    //given
+    Member.MemberContext.memberInformation.picture = 'https://example.com/profile.jpg';
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-image'));
+    // then
+    const avatarComp = screen.getByTestId('profile-image');
+    const profileImage = avatarComp.querySelector('img');
+
+    expect(profileImage).toHaveAttribute('src', 'https://example.com/profile.jpg');
+  });
+});
+
+describe('프로필 사용자 정보 렌더링', () => {
+  test('memberInformation이 빈 값이면 공백이 나온다.', async () => {
+    // given
+    const blankMemberInfo = {
+      name: '',
+      typeName: '',
+      picture: '',
+      roles: '',
+      permissions: '',
+    }
+    Member.MemberContext.memberInformation = blankMemberInfo;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-data'));
+    const memberInfoRender = screen.getByTestId('profile-data');
+    const memberInfoHeader = memberInfoRender.querySelectorAll('th');
+    const memberInfoBoday = memberInfoRender.querySelectorAll('td');
+    // then
+    expect(memberInfoRender).toBeInTheDocument();
+    // header Test
+    expect(memberInfoHeader).toHaveLength(4);
+    expect(memberInfoHeader[0]).toHaveTextContent('유형');
+    expect(memberInfoHeader[1]).toHaveTextContent('이름');
+    expect(memberInfoHeader[2]).toHaveTextContent('역할');
+    expect(memberInfoHeader[3]).toHaveTextContent('권한');
+    // body Test
+    expect(memberInfoBoday[0]).toHaveTextContent('');
+    expect(memberInfoBoday[1]).toHaveTextContent('');
+    expect(memberInfoBoday[2]).toHaveTextContent('');
+    expect(memberInfoBoday[3]).toHaveTextContent('');
+  });
+
+  test('memberInformation이 null 값이면 공백이 나온다.', async () => {
+    // given
+    Member.MemberContext.memberInformation = null;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-data'));
+    const memberInfoRender = screen.getByTestId('profile-data');
+    const memberInfoHeader = memberInfoRender.querySelectorAll('th');
+    const memberInfoBoday = memberInfoRender.querySelectorAll('td');
+
+    // then
+    expect(memberInfoRender).toBeInTheDocument();
+    // header Test
+    expect(memberInfoHeader).toHaveLength(4);
+    expect(memberInfoHeader[0]).toHaveTextContent('유형');
+    expect(memberInfoHeader[1]).toHaveTextContent('이름');
+    expect(memberInfoHeader[2]).toHaveTextContent('역할');
+    expect(memberInfoHeader[3]).toHaveTextContent('권한');
+    // body Test
+    expect(memberInfoBoday[0]).toHaveTextContent('');
+    expect(memberInfoBoday[1]).toHaveTextContent('');
+    expect(memberInfoBoday[2]).toHaveTextContent('');
+    expect(memberInfoBoday[3]).toHaveTextContent('');
+  });
+
+  test('memberInformation이 undefined 값이면 공백이 나온다.', async () => {
+    // given
+    Member.MemberContext.memberInformation = undefined;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-data'));
+    const memberInfoRender = screen.getByTestId('profile-data');
+    const memberInfoHeader = memberInfoRender.querySelectorAll('th');
+    const memberInfoBoday = memberInfoRender.querySelectorAll('td');
+
+    // then
+    expect(memberInfoRender).toBeInTheDocument();
+    // header Test
+    expect(memberInfoHeader).toHaveLength(4);
+    expect(memberInfoHeader[0]).toHaveTextContent('유형');
+    expect(memberInfoHeader[1]).toHaveTextContent('이름');
+    expect(memberInfoHeader[2]).toHaveTextContent('역할');
+    expect(memberInfoHeader[3]).toHaveTextContent('권한');
+    // body Test
+    expect(memberInfoBoday[0]).toHaveTextContent('');
+    expect(memberInfoBoday[1]).toHaveTextContent('');
+    expect(memberInfoBoday[2]).toHaveTextContent('');
+    expect(memberInfoBoday[3]).toHaveTextContent('');
+  });
+
+
+  test('memberInformation이 데이터가 존재하면 해당 데이터를 보여준다', async () => {
+    // given
+    const MemberInfo = {
+      name: '관계자',
+      typeName: '시스템',
+      picture: 'https://example.com/profile.jpg',
+      roles: ['시스템 관리자', '조직/멤버 관리자'],
+      permissions: ['MANAGE_SYSTEM_SETTINGS', 'MANAGE_MEMBERS'],
+    };
+    Member.MemberContext.memberInformation = MemberInfo;
+    // when
+    render(<Profile show onClose={() => {}} />);
+    await waitFor(() => screen.getByTestId('profile-data'));
+    const memberInfoRender = screen.getByTestId('profile-data');
+    const memberInfoHeader = memberInfoRender.querySelectorAll('th');
+    const memberInfoBoday = memberInfoRender.querySelectorAll('td');
+
+    // then
+    expect(memberInfoRender).toBeInTheDocument();
+    // header Test
+    expect(memberInfoHeader).toHaveLength(4);
+    expect(memberInfoHeader[0]).toHaveTextContent('유형');
+    expect(memberInfoHeader[1]).toHaveTextContent('이름');
+    expect(memberInfoHeader[2]).toHaveTextContent('역할');
+    expect(memberInfoHeader[3]).toHaveTextContent('권한');
+    // body Test
+    expect(memberInfoBoday[0]).toHaveTextContent('시스템');
+    expect(memberInfoBoday[1]).toHaveTextContent('관계자');
+    expect(memberInfoBoday[2]).toHaveTextContent('시스템 관리자');
+    expect(memberInfoBoday[2]).toHaveTextContent('조직/멤버 관리자');
+    expect(memberInfoBoday[3]).toHaveTextContent('MANAGE_SYSTEM_SETTINGS');
+    expect(memberInfoBoday[3]).toHaveTextContent('MANAGE_MEMBERS');
+  });
+});
+// describe('onClose 함수 호출', () => {
+//   test('member context에 picture가 빈 값이면 디폴트 이미지가 나온다.', async () => {
+//     // given
+
+//     // when
+
+//     // then
+
+//   });
+
+// });
